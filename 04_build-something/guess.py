020c65f7570438959c0d10b8c10620fec4db04f1
Daniel Cerda Escobar <danielcerda@Daniels-MacBook-Air.local>
2024-07-20 15:30:36 -0400

    Guess a number python script

--- 04_build-something/guess.py
+++ 04_build-something/guess.py
@@ -8,3 +8,18 @@
 # Type the whole code out instead of copy-pasting.
 # Typing out code, even if you just copy it, trains your coding skills!
 # Write your code below:
+
+import random
+
+num = random.randint(0,10)
+guess = None
+
+while guess!=num:
+    guess = input('Guess a number between 0 and 10 :')
+    guess = int(guess)
+    if guess==num:
+        print('Congratulations, you won!')
+        break
+    else:
+        print('Nope, try again')
+    
