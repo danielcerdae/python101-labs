0e436c6b849d27507d56063a467f1258c1d0dddc
Daniel Cerda Escobar <danielcerda@Daniels-MacBook-Air.local>
2024-07-20 15:38:34 -0400

    Update first python labs

--- 05_machine-setup/05_04_seconds_year.py
+++ 05_machine-setup/05_04_seconds_year.py
@@ -1,2 +1,6 @@
 # Use the interpreter to calculate how many seconds are in a year.
 # Then write the code in this script file below this line.
+
+sec = 365*24*3600
+
+print(f'{sec} seconds in a year!')
