963640b08aadc754f0e445c7dd445e91b311d59b
Daniel Cerda Escobar <danielcerda@Daniels-MacBook-Air.local>
2024-07-28 22:17:45 -0400

    Intro to programming labs

--- 06_intro-to-programming/06_01_pseudocode.py
+++ 06_intro-to-programming/06_01_pseudocode.py
@@ -6,3 +6,7 @@
 # Break this task up into a couple of steps of pseudocode
 # and write the pseudocode below in code comments.
 # You don't need to write any functional code, just map out the steps.
+
+# Save all the names of the kittens in a list
+# Iterate for every name on the list
+# Inside the for loop print a message that each kitten is ready for adoption
