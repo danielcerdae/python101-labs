e2a29467309198fc0fdbb5fc3b36c08952ce77a0
Daniel Cerda Escobar <danielcerda@Daniels-MacBook-Air.local>
2024-10-13 21:25:37 -0300

    Lab Strings and Text

--- 09_strings-and-text/09_01_longest.py
+++ 09_strings-and-text/09_01_longest.py
@@ -5,3 +5,13 @@ longest_german_word = "Donaudampfschifffahrtsgesellschaftskapitänskajütentürs
 longest_hungarian_word = "Megszentségteleníthetetlenségeskedéseitekért"
 longest_finnish_word = "Lentokonesuihkuturbiinimoottoriapumekaanikkoaliupseerioppilas"
 strong_password = "%8Ddb^ca<*'{9pur/Y(8n}^QPm3G?JJY}\(<bCGHv^FfM}.;)khpkSYTfMA@>N"
+
+word_1 = len(longest_german_word)
+
+word_2 = len(longest_hungarian_word)
+
+word_3 = len(longest_finnish_word)
+
+word_4 = len(strong_password)
+
+print(f'{word_1},  {word_2}, {word_3}, {word_4}')
